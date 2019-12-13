6496c05b3c3d60ed4978f4cd93d2ff732821d60f
Keenan Smith <keenan.smith500@gmail.com>
2019-12-12 22:49:49 -0500

    Create solution

---
+++ MetricsCollector/MetricsCollector/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MetricsCollector.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
